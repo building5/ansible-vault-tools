f749e38706440b34dbd81c913291452b2a2fff2b
David M. Lee <leedm777@yahoo.com>
2016-06-16 15:04:06 -0500

    gpg-vault-password-file: Allow overwrites
    The script was cumbersome if the password file already existed. Instead
    of being paranoid about overwriting it, we'll now read/execute the file,
    and encrypt whatever those results are.

--- CHANGES.md
+++ CHANGES.md
@@ -1,5 +1,11 @@
 # ansible-vault-tools changelog
 
+## v2.0.0 (2016-06-16)
+
+ * Change `gpg-vault-password-file` so that if you give it a file that already
+   exists, it will get the password from that, and encrypt it. Makes it easier
+   to add encryption to existing password file.
+
 ## v1.0.1 (2016-01-26)
 
  * #1 - Updated README
--- gpg-vault-password-file.sh
+++ gpg-vault-password-file.sh
@@ -7,7 +7,7 @@
 PROGNAME=$(basename $0)
 
 usage() {
-    cat <<EOF
+  cat <<EOF
 usage: ${PROGNAME} [OPTION]... [--] new-password-file
 
   -h, --help Display this help
@@ -15,66 +15,76 @@ EOF
 }
 
 while test $# -gt 0; do
-    case $1 in
-        --help|-h)
-            usage
-            exit 0
-            ;;
-        --)
-            shift 1
-            break
-            ;;
-        -*)
-            echo "${PROGNAME}: unknown option $1" >&2
-            usage >&2
-            exit 1
-            ;;
-        *)
-            # probably the first positional argument
-            break
-    esac
+  case $1 in
+    --help|-h)
+      usage
+      exit 0
+      ;;
+    --)
+      shift 1
+      break
+      ;;
+    -*)
+      echo "${PROGNAME}: unknown option $1" >&2
+      usage >&2
+      exit 1
+      ;;
+    *)
+      # probably the first positional argument
+      break
+  esac
 done
 
 if test $# -lt 1; then
-    echo "${PROGNAME}: not enough arguments" >&2
-    usage >&2
-    exit 1
+  echo "${PROGNAME}: not enough arguments" >&2
+  usage >&2
+  exit 1
 fi
 
 PASSWORD_SCRIPT=$1
 PASSWORD_CIPHERTEXT=$1.asc
 
-MARKER_COMMENT="# !!! Written by gpg-vault-password-file !!!"
-
+# Get the old password, if there is one
 if test -e ${PASSWORD_SCRIPT}; then
-    if ! grep -q -F "${MARKER_COMMENT}" ${PASSWORD_SCRIPT}; then
-        echo "${PROGNAME}: refusing to overwrite ${PASSWORD_SCRIPT}" >&2
-        exit 1
-    fi
+  if test -x ${PASSWORD_SCRIPT}; then
+    # existing password is a script. Run it!
+    OLD_VAULT_PASSWORD="$(${PASSWORD_SCRIPT})"
+  elif test -f ${PASSWORD_SCRIPT}; then
+    # existing password is plain. Cat it!
+    OLD_VAULT_PASSWORD="$(cat ${PASSWORD_SCRIPT})"
+  else
+    echo "${PROGNAME}: ${PASSWORD_SCRIPT} is not a file" >&2
+    exit 1
+  fi
 
-    OLD_VAULT_PASSWORD=$(${PASSWORD_SCRIPT})
+  if test $? -ne 0; then
+    echo "${PROGNAME}: failed to read ${PASSWORD_SCRIPT}" >&2
+    exit 1
+  fi
 else
-    # If the ciphertext exists, but not the script
-    if test -e ${PASSWORD_CIPHERTEXT}; then
-        echo "${PROGNAME}: refusing to overwrite ${PASSWORD_CIPHERTEXT}" >&2
-        exit 1
-    fi
+  # If the ciphertext exists, but not the script
+  if test -e ${PASSWORD_CIPHERTEXT}; then
+    echo "${PROGNAME}: refusing to overwrite ${PASSWORD_CIPHERTEXT}" >&2
+    exit 1
+  fi
 fi
 
+# Get the new password, if there is one
+echo "Press Ctrl-c to abort without changing anything"
 read -s -p "Enter vault password [$(echo ${OLD_VAULT_PASSWORD} | sed 's/.*\(...\)/***\1/')]: " VAULT_PASSWORD
 echo
 if test -z "${VAULT_PASSWORD}"; then
-    VAULT_PASSWORD="${OLD_VAULT_PASSWORD}"
+  VAULT_PASSWORD="${OLD_VAULT_PASSWORD}"
 fi
 
 if test -z "${VAULT_PASSWORD}"; then
-    echo "${PROGNAME}: empty password" >&2
-    exit 1
+  echo "${PROGNAME}: empty password" >&2
+  exit 1
 fi
 
 cat <<EOF > ${PASSWORD_SCRIPT}
 #!/bin/sh
-${MARKER_COMMENT}
+# !!! Written by gpg-vault-password-file !!!
 gpg --decrypt --batch < \$0.asc 2> /dev/null
 EOF
 chmod a+x ${PASSWORD_SCRIPT}
